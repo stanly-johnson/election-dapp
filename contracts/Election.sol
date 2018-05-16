17bf05639ea3187d714c664c1aba05ab4ba86114
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-16 20:23:48 +0530

    directorychange

---
+++ contracts/Election.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.17;
+
+contract Election {
+
+    //candidate model
+    struct Candidate {
+      uint id;
+      string name;
+      uint voteCount;
+    }
+
+    //store candidate data
+    mapping(uint => Candidate) public candidates;
+
+    //store voter data
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+    event votedEvent (
+      uint indexed _candidateId
+      );
+
+    //contructor
+    function Election () public {
+      addCandidate("John Doe");
+      addCandidate("Another John Doe");
+    }
+
+    function addCandidate (string _name) private {
+      candidatesCount++;
+      candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+      //check if the address has voted before
+      require(!voters[msg.sender]);
+
+      //check if the candidate is valid
+      require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+      //mark the user as voted
+      voters[msg.sender] = true;
+
+      //update voteCount
+      candidates[_candidateId].voteCount++;
+
+      //trigger voted event
+      votedEvent(_candidateId);
+
+    }
+
+}
