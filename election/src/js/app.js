f9a7386efd02c6a534bfb4df2ed557f758ed2610
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-16 20:06:21 +0530

    webapp-done

--- election/src/js/app.js
+++ election/src/js/app.js
@@ -2,24 +2,23 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
+  hasVoted: false,
 
   init: function() {
-    // initialize web3
     return App.initWeb3();
   },
 
   initWeb3: function() {
 
-    if(typeof web3 !== 'undefined'){
-      //If a web3 instance already exists
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already present
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
-    }
-    else{
+    } else {
+      // Specify default instance
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
-
     return App.initContract();
   },
 
@@ -58,6 +57,9 @@ App = {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
       for (var i = 1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
@@ -67,13 +69,35 @@ App = {
           // Render candidate Result
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
         });
       }
-
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+      }
       loader.hide();
       content.show();
     }).catch(function(error) {
       console.warn(error);
+    });
+  },
+
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
     });
   }
 };
