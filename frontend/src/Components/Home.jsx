0fef8825c2270f9ceb1138cd0842e2c5a0ff15ed
Stanly Johnson <stanly@sovereignwallet.network>
2020-06-20 08:37:51 +0530

    latest changes

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,82 +1,116 @@
 import React, { Component } from 'react';   
 import { Button } from 'react-bootstrap';
 import Web3 from 'web3'
-import { CONTRACT_ABI, CONTRACT_ADDRESS, INFURA_PROVIDER, SENDER_ADDRESS, SENDER_PVT_KEY } from './constants'
-import CandidateTable from './CandidateTable'
-var Tx = require('ethereumjs-tx');
+import TruffleContract from 'truffle-contract'
+import Election from './contracts/Election.json'
+import Content from './Content'
+import 'bootstrap/dist/css/bootstrap.css'
 
 class Home extends Component {
-    state = { 
-        account : "",
-        loading : true,
-        candidatesCount : "",
-        candidateList : []
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0',
+      candidates: [],
+      hasVoted: false,
+      loading: true,
+      voting: false,
+    }
+    
+    window.addEventListener('load', function () {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try { 
+           window.ethereum.enable().then(function() {
+               // User has allowed account access to DApp...
+               //alert('You can now use the DApp !');
+           });
+        } catch(e) {
+           // User has denied account access to DApp...
+           alert('You have denied access to DApp !');
+        }
      }
+     // Legacy DApp Browsers
+     else if (window.web3) {
+         const web3 = new Web3(window.web3.currentProvider);
+     }
+     // Non-DApp Browsers
+     else {
+         alert('You have to install MetaMask !');
+    }  
+    });
 
-    fetchBlockchainData = async() => {
-      const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_PROVIDER));
-      var getNonce = await web3.eth.getTransactionCount(SENDER_ADDRESS, 'pending');
-      var contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS, { from: SENDER_ADDRESS });
-      const payload = contract.methods.createCandidate(this.state.name, this.state.college, this.state.addr, this.state.phone).encodeABI();
-      // prepare transaction
-      var rawTx = {
-          nonce: getNonce,
-          gasPrice: web3.utils.toHex('10000000000'),
-          gasLimit: web3.utils.toHex('3000000'),
-          to: CONTRACT_ADDRESS,
-          value: '0x0',
-          data: payload
-      };
+    this.web3 = new Web3(window.web3.currentProvider)
+    this.election = TruffleContract(Election)
+    this.election.setProvider(window.web3.currentProvider)
 
-      console.log(rawTx);
-      var tx = new Tx(rawTx);
-      var privKey = Buffer.from(SENDER_PVT_KEY, 'hex');
-      tx.sign(privKey);
+    this.castVote = this.castVote.bind(this)
+    this.watchEvents = this.watchEvents.bind(this)
 
-      var serializedTx = tx.serialize();
+  }
 
-      web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
-          if (!err) {
-              console.log(hash);
-          } else {
-              console.log(err)
+  componentDidMount() {
+    // TODO: Refactor with promise chain
+    this.web3.eth.getCoinbase((err, account) => {
+      this.setState({ account })
+      this.election.deployed().then((electionInstance) => {
+        this.electionInstance = electionInstance
+        this.watchEvents()
+        this.electionInstance.candidatesCount().then((candidatesCount) => {
+          for (var i = 1; i <= candidatesCount; i++) {
+            this.electionInstance.candidates(i).then((candidate) => {
+              const candidates = [...this.state.candidates]
+              candidates.push({
+                id: candidate[0],
+                name: candidate[1],
+                voteCount: candidate[2]
+              });
+              this.setState({ candidates: candidates })
+            });
           }
+        })
+        this.electionInstance.voters(this.state.account).then((hasVoted) => {
+          this.setState({ hasVoted, loading: false })
+        })
       })
-    }
+    })
+  }
 
-    async componentDidMount() {
-        this.fetchBlockchainData()
-    }
+  watchEvents() {
+    this.electionInstance.votedEvent({}, {
+      fromBlock: 0,
+      toBlock: 'latest'
+    }).watch((error, event) => {
+      this.setState({ voting: false })
+    })
+  }
 
-    render() { 
-        return (
-        <React.Fragment>
-            <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">Home</h1>
-                <p>Active Account : {this.state.account}</p>
-                <button type="button" class="btn btn-primary" onClick={() => {
-              window.location = '/add';
-            }}>Add New Student</button>
-            </div>
+  castVote(candidateId) {
+    this.setState({ voting: true })
+    this.electionInstance.vote(candidateId, { from: this.state.account }).then((result) =>
+      this.setState({ hasVoted: true })
+    )
+  }
 
-            <div className="row">
-                {this.state && !this.state.loading && (
-                <div className="col-md-6">
-                {this.state.candidateList.map(c => (
-                  <ul key={c.uid}>
-                    <CandidateTable c={c} />
-                  </ul>
-                ))}
-              </div>
-              )}
-            </div>
-
-            </div>
-
-        </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <div class='row'>
+        <div class='col-lg-12 text-center' >
+          <br /><br /><br />
+          <h1>Decentralised Voting</h1>
+          <br/>
+          { this.state.loading || this.state.voting
+            ? <p class='text-center'>Loading...</p>
+            : <Content
+                account={this.state.account}
+                candidates={this.state.candidates}
+                hasVoted={this.state.hasVoted}
+                castVote={this.castVote} />
+          }
+        </div>
+      </div>
+    )
+  }
 }
  
 export default Home;
